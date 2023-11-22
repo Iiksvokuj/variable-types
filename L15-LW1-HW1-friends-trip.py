f1f9f2bead21b73c91e4189698a5a204ffe3b098
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-22 21:26:59 +0200

    HW1 and HW2*:  ~~ done

--- L15-LW1-HW1-friends-trip.py
+++ L15-LW1-HW1-friends-trip.py
@@ -3,7 +3,7 @@ me_and_my_friend       = {"Dorin", "Radjamhathaprajapatijaayeshkumar"}
 my_friends             = ["John", "Marry", "Jane"]
 my_best_friend_friends = ("Marry", "Pete", "Oliver", "Mia")
 
-people_to_exclude      = {"Vasilii"}
+people_to_exclude      = {"Nobody"}
 exlude_count           = 0
 
 from os import system
@@ -12,12 +12,12 @@ print("Don't wanna go? Enter your name below")
 while True:
     exlude_count += 1
     exclude_name = input(str(exlude_count) + ") >> ")
-    people_to_exclude.discard("Vasilii")
+    people_to_exclude.discard("Nobody")
     people_to_exclude.add(exclude_name)
     if exclude_name == "":
         break
-
 system('cls')
+
 ########################################
 trip_people_list = me_and_my_friend.union(
     my_friends,
@@ -25,6 +25,7 @@ trip_people_list = me_and_my_friend.union(
 )
 
 trip_people_list = trip_people_list.difference(people_to_exclude)
+trip_people_list = list(trip_people_list)
 
 print(len(trip_people_list), "are going along:")
 
@@ -38,12 +39,11 @@ print(len(trip_people_list), "are going along:")
 # HW2*: 
 #    enter the manes of people to exlude from the keyboard
 
-for participant in trip_people_list:
-    print(participant)
-print(
-"""
-"""
-)
+print()
+for participant in range(len(trip_people_list)):
+    print(participant+1,") ", trip_people_list[participant])
 
-# for name in people_to_exclude:
-#     print(name)
+print()
+print(len(people_to_exclude)-1, "won't go:")
+for name in people_to_exclude:
+    print(name)
