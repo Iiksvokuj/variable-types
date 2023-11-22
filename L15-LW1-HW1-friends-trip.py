0b3e631dc746419bed943e05ddce1255b138b40b
zhukovskiyivan <150742832+Iiksvokuj@users.noreply.github.com>
2023-11-22 21:59:37 +0200

    fixed. added : people_to_exclude  = set()

--- L15-LW1-HW1-friends-trip.py
+++ L15-LW1-HW1-friends-trip.py
@@ -3,7 +3,8 @@ me_and_my_friend       = {"Dorin", "Radjamhathaprajapatijaayeshkumar"}
 my_friends             = ["John", "Marry", "Jane"]
 my_best_friend_friends = ("Marry", "Pete", "Oliver", "Mia")
 
-people_to_exclude      = {"Nobody"}
+# people_to_exclude      = {"Nobody"}
+people_to_exclude      = set()
 exlude_count           = 0
 
 from os import system
@@ -12,7 +13,7 @@ print("Don't wanna go? Enter your name below")
 while True:
     exlude_count += 1
     exclude_name = input(str(exlude_count) + ") >> ")
-    people_to_exclude.discard("Nobody")
+#    people_to_exclude.discard("Nobody")
     people_to_exclude.add(exclude_name)
     if exclude_name == "":
         break
